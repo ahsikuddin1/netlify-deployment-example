e523840304d49dc10f50967c78a8eccf32809f78
soleilyasmina <mounlasolomon@gmail.com>
2020-08-31 14:40:35 -0400

    move state and examples;

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import Team from './Team';
 import './App.css';
 
 function App() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const response = await axios.get('https://pacific-hamlet-09299.herokuapp.com/teams');
+      setTeams(response.data);
+      console.log('teams achieved')
+    }
+    apiCall();
+    return () => console.log('team will unmount!');
+  }, []);
+
   return (
     <div className="App">
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/team/Bits Please">Bits Please</Link>
         <Link to="/team/Smacké">Smacké</Link>
         <Link to="/team/Breaking Bad Habits">Breaking Bad Habits</Link>
         <Link to="/team/Team Bleu">Team Bleu</Link>
       </nav>
       <main>
-        <Route path="/team/:name">
-          <Team />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <h3>Home!</h3>
+          </Route>
+          <Route path="/team/:banana">
+            <Team teams={teams} />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/Team.jsx
+++ src/Team.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import teams from './teams.json';
-
-export default function Team() {
+export default function Team(props) {
   const params = useParams();
-  console.log(params.name)
 
-  const team = teams.find((team) => params.name === team.name);
-  console.log(team);
+  useEffect(() => {
+    console.log('heyo sandy what is GOOD my man')
+  }, [])
+
+  // doesn't do anything, just a .find() example
+  const secondTeam = props.teams.find((team, index) => index === 1);
+  console.log(secondTeam);
+  const team = props.teams.find((team) => params.banana === team.name);
 
   return (
     <div>
-      <h3>They are known as... {team.name}</h3>
-      <h4>Current Score: {team.score}</h4>
-      <ul>
-        {
-          team.members.map((item, index) => (<li key={index}>{item}</li>))
-        }
-      </ul>
+      {team
+        ? (
+          <>
+            <h3>They are known as... {team.name}</h3>
+            <h4>Current Score: {team.score}</h4>
+            <ul>
+              {
+                team.members.map((item, index) => (<li key={index}>{item}</li>))
+              }
+            </ul>
+          </>
+        )
+        : <h3>No team found!</h3>
+      }
+
     </div>
   )
 }
