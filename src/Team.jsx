1041bb3f91337eae27cc98faca3513fbac0bc50f
soleilyasmina <mounlasolomon@gmail.com>
2020-09-01 11:43:04 -0400

    formatting

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,35 +1,39 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Team(props) {
   const params = useParams();
 
   useEffect(() => {
-    console.log('heyo sandy what is GOOD my man')
-  }, [])
+    console.log("heyo sandy what is GOOD my man");
+  }, []);
+
+  // same as const teams = props.teams;
+  const { teams } = props;
 
   // doesn't do anything, just a .find() example
-  const secondTeam = props.teams.find((team, index) => index === 1);
+  const secondTeam = teams.find((team, index) => index === 1);
   console.log(secondTeam);
-  const team = props.teams.find((team) => params.banana === team.name);
+  const team = teams.find((team) => params.banana === team.name);
 
   return (
     <div>
-      {team
-        ? (
-          <>
-            <h3>They are known as... {team.name}</h3>
-            <h4>Current Score: {team.score}</h4>
-            <ul>
-              {
-                team.members.map((item, index) => (<li key={index}>{item}</li>))
-              }
-            </ul>
-          </>
-        )
-        : <h3>No team found!</h3>
-      }
-
+      {team ? (
+        <>
+          <h3>They are known as... {team.name}</h3>
+          <h4>
+            Current Score:
+            {team.score}
+          </h4>
+          <ul>
+            {team.members.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h3>No team found!</h3>
+      )}
     </div>
-  )
+  );
 }
